7478e0953c39558ee4695b57cad070be8c090ad5
Alexander Foley <alexandergfoley@gmail.com>
2023-07-10 11:04:18 -0600

    chore: Adding cast

--- record.sh
+++ record.sh
@@ -4,3 +4,5 @@ cd examples
 docker-compose up -d
 curl -s http://truck.localhost:2002 | grep -op 'name.*$' | sed -e 's/<[^>]*>//g'
 curl -s http://car.localhost:2002 | grep -op 'name.*$' | sed -e 's/<[^>]*>//g'
+
+asciicast2gif -w 80 -h 30 img/demo.cast img/democast.gif
