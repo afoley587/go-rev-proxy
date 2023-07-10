643ba0026c67e6fd6ae79ec9e969c9883eb20139
Alexander Foley <alexandergfoley@gmail.com>
2023-07-10 09:15:39 -0600

    adding comments

--- proxy.go
+++ proxy.go
@@ -9,15 +9,16 @@ import (
 	"net/url"
 )
 
-const (
-	ReverseServerAddr    = "0.0.0.0:2002"                // this is our reverse server ip address
-	InsideProxyHostname  = "proxy:2002"                  // Requests from private network
-	OutsideProxyHostname = "registration.localhost:2002" // Requests from public network
+var (
+	RunPort              = 2002                                           // The server port to run on
+	ReverseServerAddr    = fmt.Sprint("0.0.0.0:", RunPort)                // this is our reverse server ip address
+	InsideProxyHostname  = fmt.Sprint("proxy:", RunPort)                  // Requests from private network
+	OutsideProxyHostname = fmt.Sprint("registration.localhost:", RunPort) // Requests from public network
+	KnownAddresses       = map[string]string{}                            // Known Addresses
 )
 
-// Addresses that have been registered with us
-var KnownAddresses = map[string]string{}
-
+// This function checks if TLS was enabled on the request
+// and translates it to the proper scheme (http or https)
 func GetScheme(c *gin.Context) string {
 	if c.Request.TLS != nil {
 		return "https"
@@ -26,12 +27,20 @@ func GetScheme(c *gin.Context) string {
 	}
 }
 
+// IsRegistrationRequest checks if an incoming request is meant to register
+// a new inside/outside hostname pair by checking if the hostname and 
+// path match a specific combination.
+// If the host is our registration.localhost or proxy hostname
+// AND if the path is /register, we will register a new endpoint
 func IsRegistrationRequest(c *gin.Context) bool {
 	isRR := ((c.Request.Host == InsideProxyHostname || c.Request.Host == OutsideProxyHostname) &&
 		c.Request.URL.String() == "/register")
 	return isRR
 }
 
+// SaveRegistrationRequest saves an inside/outside hostname pairing
+// to our KnownAddresses map so we can use it later. Effectively
+// registering a new endpoint for us
 func SaveRegistrationRequest(c *gin.Context) error {
 	var rr RegistrationRequest
 	log.Println(c.Request.Body)
@@ -46,60 +55,77 @@ func SaveRegistrationRequest(c *gin.Context) error {
 	return nil
 }
 
-func main() {
-	r := gin.Default()
+// Proxy runs the actual proxy and will look at the 
+// hostnames requested from the received request. It will
+// then translate that to the inside hostname and forward the
+// request
+func Proxy(c *gin.Context) {
 
-	r.Any("/*path", func(c *gin.Context) {
+	// Get if HTTP or HTTPS
+	scheme := GetScheme(c)
 
-		scheme := GetScheme(c)
+	log.Println(scheme, c.Request.Host, c.Request.URL.String())
 
-		log.Println(scheme, c.Request.Host, c.Request.URL.String())
+	// If this is a registration request, save it and
+	// then stop processing this request
+	if IsRegistrationRequest(c) {
 
-		if IsRegistrationRequest(c) {
-
-			err := SaveRegistrationRequest(c)
-
-			if err != nil {
-				log.Println(err)
-				c.String(400, "Couldnt Register Host")
-				return
-			}
-
-			c.String(201, "Host Registered")
-			return
-		}
-
-		forwardTo, ok := KnownAddresses[c.Request.Host]
-
-		if !ok {
-			log.Printf("Unkown Host: %v", c.Request.Host)
-			c.String(400, "Unkown Host")
-			return
-		}
-
-		rUrl := fmt.Sprintf("%v://%v%v", scheme, forwardTo, c.Request.URL)
-
-		remote, err := url.Parse(rUrl)
+		err := SaveRegistrationRequest(c)
 
 		if err != nil {
 			log.Println(err)
-			c.String(500, "Error Proxying Host")
+			c.String(400, "Couldnt Register Host")
+			return
 		}
 
-		log.Println("Forwarding request to", remote)
+		c.String(201, "Host Registered")
+		return
+	}
 
-		proxy := httputil.NewSingleHostReverseProxy(remote)
+	// Translate the outside hostname to the inside hostname
+	forwardTo, ok := KnownAddresses[c.Request.Host]
 
-		proxy.Director = func(req *http.Request) {
-			req.Header = c.Request.Header
-			req.Host = remote.Host
-			req.URL.Scheme = remote.Scheme
-			req.URL.Host = remote.Host
-			req.URL.Path = c.Param("path")
-		}
+	if !ok {
+		log.Printf("Unkown Host: %v", c.Request.Host)
+		c.String(400, "Unkown Host")
+		return
+	}
 
-		proxy.ServeHTTP(c.Writer, c.Request)
-	})
+	rUrl := fmt.Sprintf("%v://%v%v", scheme, forwardTo, c.Request.URL)
+
+	remote, err := url.Parse(rUrl)
+
+	if err != nil {
+		log.Println(err)
+		c.String(500, "Error Proxying Host")
+		return
+	}
+
+	log.Println("Forwarding request to", remote)
+
+	// Forward the request to the inside remote server
+	// https://pkg.go.dev/net/http/httputil#NewSingleHostReverseProxy
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+
+	// Director is a function which modifies
+	// the request into a new request to be sent
+	// https://pkg.go.dev/net/http/httputil#ReverseProxy
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = remote.Host
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+		req.URL.Path = c.Param("path")
+	}
+
+	proxy.ServeHTTP(c.Writer, c.Request)
+}
+
+func main() {
+	r := gin.Default()
+
+	// all paths should be handled by Proxy()
+	r.Any("/*path", Proxy)
 
 	if err := r.Run(ReverseServerAddr); err != nil {
 		log.Printf("Error: %v", err)
--- registrationrequest.go
+++ registrationrequest.go
@@ -1,6 +1,6 @@
 package main
 
 type RegistrationRequest struct {
-	OutsideHost string
-	InsideHost  string
+	OutsideHost string // Outside host address. The hostname or IP on the public network. For example foo.bar.com or foo.localhost
+	InsideHost  string // Inside host address. The hostname or IP on the internal network. For example 192.168.10.5
 }
